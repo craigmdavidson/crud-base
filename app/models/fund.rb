f9ea1559727ef279fbc4837b47262486392ea496
Craig Davidson <craigmdavidson@gmail.com>
2026-03-12 12:40:24 +0000

    key attributes only in a grid at the top and details in a tab below

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -3,7 +3,7 @@ class Fund < ApplicationRecord
   
   KEY_ATTRIBUTES = [:name, :organization_id, :start_date, :end_date, :total_fund]  
   
-  has_auto_controller after_save_redirect_to: :index, key_attributes: KEY_ATTRIBUTES
+  has_auto_controller key_attributes: KEY_ATTRIBUTES
   has_nested_auto_controller Organization, key_attributes: KEY_ATTRIBUTES
 
   
