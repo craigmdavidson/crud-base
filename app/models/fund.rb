3d83e8975d57544f8852c1298525138a845c9453
Craig Davidson <craigmdavidson@gmail.com>
2026-03-15 11:21:23 +0000

    ActiveRecord helper to wrap composed_of relationship

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -1,10 +1,7 @@
 class Fund < ApplicationRecord
   belongs_to :organization
 
-  composed_of :total_fund, class_name: "Auto::Money", mapping: [%w[total_fund_amount amount], %w[total_fund_currency currency]], allow_nil: true, converter: :parse
-  composed_of :research_allocation, class_name: "Auto::Money", mapping: [%w[research_allocation_amount amount], %w[research_allocation_currency currency]], allow_nil: true, converter: :parse
-  composed_of :operations_expense_allocation, class_name: "Auto::Money", mapping: [%w[operations_expense_allocation_amount amount], %w[operations_expense_allocation_currency currency]], allow_nil: true, converter: :parse
-  composed_of :program_delivery_allocation, class_name: "Auto::Money", mapping: [%w[program_delivery_allocation_amount amount], %w[program_delivery_allocation_currency currency]], allow_nil: true, converter: :parse
+  money :total_fund, :research_allocation, :operations_expense_allocation, :program_delivery_allocation
 
   KEY_ATTRIBUTES = [:name, :organization_id, :start_date, :end_date, :total_fund]
 
